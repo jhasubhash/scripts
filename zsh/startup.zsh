7f05baa335d8537105cacdcb659c331c3fe7b8d1
subhash jha <subhashjha@subhashs-MacBook-Pro-2.local>
2021-02-10 22:22:47 +0530

    comment code

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -3,8 +3,13 @@ alias reload="source ~/.zshrc"
 alias gs="git status"
 alias gaa="git add ."
 alias cdd="cd ~/Documents"
+alias ..="cd .."
+alias ...="cd ../.."
+alias .3="cd ../../.."
+alias .4="cd ../../../.."
+alias modzsh="vim ~/scripts/zsh/startup.zsh" 
 
 #configure emsdk
-CURRPATH=${$(pwd)}
-cd /home/subhash/Documents/work/emsdk/ && source emsdk_env.sh
-cd $CURRPATH
+#CURRPATH=${$(pwd)}
+#cd ~/Documents/work/emsdk/ && source emsdk_env.sh
+#cd $CURRPATH
